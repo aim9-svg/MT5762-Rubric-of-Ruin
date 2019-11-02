2eb5ef385858b37e35d2afaf4905f369716f28ee
Yuankun <56158066+Yuankun-Cheng@users.noreply.github.com>
2019-11-02 16:03:24 +0000

    Update modelselection.R

--- modelselection.R
+++ modelselection.R
@@ -219,10 +219,11 @@ lm.f2.foi <- lm(baby_wt~(gestation+m_race+m_ht+d_race+d_wt+smoke+previous_preg)^
 lmsf2 <- step(lm.f2.foi)
 summary(lmsf2)
 # Plot the results of the interactions
-plot(effect("gestation:m_race", lmsf2), multiline=TRUE)
+plot(effect("gestation:smoke", lmsf2), multiline=TRUE)
 plot(effect("gestation:previous_preg", lmsf2), multiline=TRUE)
+plot(effect("d_wt:smoke", lmsf2), multiline=TRUE)
 
-#2. Consider all first-order interactions in 'lm.f3' model
+#3. Consider all first-order interactions in 'lm.f3' model
 lm.f3.foi <- lm(baby_wt~(gestation+m_race+m_ht+m_wt+d_race+d_wt+smoke+previous_preg)^2, data = lmData )
 # Use stepwise method to improve the model
 lmsf3 <- step(lm.f3.foi)
