8a5331a69ec517712675cf57c168236e47bc6c52
JingyiLi318 <56074065+JingyiLi318@users.noreply.github.com>
2019-11-02 11:28:38 +0000

    change some plots and add comments

--- MT5762 - Assignment 2.R
+++ MT5762 - Assignment 2.R
@@ -5,16 +5,20 @@ install.packages("tidyverse")
 install.packages("randomForest")
 install.packages("mice")
 install.packages("ggplot2")
+install.packages("Rmisc")
 library(tidyverse)
 library(dplyr)
 library(mice)
 library(randomForest)
 library(ggplot2)
+library(Rmisc)
+
 
 ## Import data 
 babies <- readxl::read_xlsx("Original Data.xlsx")
 
 ## Clean data
+# delete some variables which are independent of the response variable baby_wt.
 clean_babies <- babies %>%
   select(-id, -pluralty, -outcome, -date, -sex) %>%
   rename(baby_wt = wt...7, previous_preg = parity, m_race = race, m_age = age,
@@ -53,18 +57,31 @@ full_df = na.omit(clean_babies)
 clean_babies <- clean_babies[,c(na_col,full_col)]
 
 ##--------------first method: fill the missing value-----------------------------##
+# Mice(Multivariate Imputation by Chained Equations) method first uses mice function modeling and then USES complete function to generate complete data.The mice(df) operation returns multiple complete copies of the df, each with a different value for the missing data.The complete() function returns one (default) or more of these data sets.
+
 # Fill in the missing value with mice interpolation based on random forest model
 #miceMod <- mice(clean_babies[, !names(clean_babies) %in% "baby"], method="rf") 
 # Generate complete data
 #babies_data <- complete(miceMod)  
 # Check for missing values in the dataset
 #anyNA(babies_data)
+##--------------------------------------------------------------------------------##
+
+# mice method of filling in missing values is the most reasonable one we can find, 
+# but there is still a chance of filling in the wrong values, 
+# so we decided to delete the missing values.
+
 
 ##-------------second method: delete the missing value----------------------------##
-babies_data <- na.omit(clean_babies)
+# If the data set to be regressed has a large number of observations enough to be used to build the model, 
+# it is a reasonable method to delete the observations containing missing values, 
+# as long as enough sample points can be used for modeling after the observations are deleted.
 
+babies_data <- na.omit(clean_babies)
 # Check for missing values in the dataset
 anyNA(babies_data)
+
+##--------------------------------------------------------------------------------##
 
 
 ## Redefine some factor variables(divided by group) to creat dummy variables
@@ -131,125 +148,112 @@ babies_data$cig_number[babies_data$cig_number == 6] = "30-39"
 babies_data$cig_number[babies_data$cig_number == 7] = "40-60"
 babies_data$cig_number[babies_data$cig_number == 8] = "60+"
 
-#income
-?????
 
-
-##  Explore data 
+##  Explore relationships between each predictor variable and the response variable.
 # relationship: baby weight and gestation
-ggplot(babies_data,aes(gestation,baby_wt))+
+p_wt_ges <- ggplot(babies_data,aes(gestation,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_gestation <-cor.test(babies_data$gestation,babies_data$baby_wt)
 
 # relationship: baby weight and m_race
-ggplot(babies_data,aes(m_race,baby_wt))+
+p_wt_mrace <- ggplot(babies_data,aes(m_race,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
-# Calculate the correlation coefficient and test whether the correlation is significant
-bbwt_m_race <-cor.test(babies_data$m_race,babies_data$baby_wt) 
 
 # relationship: baby weight and m_age
-ggplot(babies_data,aes(m_age,baby_wt))+
+p_wt_mage <- ggplot(babies_data,aes(m_age,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_m_age <-cor.test(babies_data$m_age,babies_data$baby_wt) 
 
 # relationship: baby weight and m_edu
-ggplot(babies_data,aes(m_edu,baby_wt))+
+p_wt_medu <- ggplot(babies_data,aes(m_edu,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
-# Calculate the correlation coefficient and test whether the correlation is significant
-bbwt_m_edu <-cor.test(babies_data$m_edu,babies_data$baby_wt) 
 
 # relationship: baby weight and m_ht
-ggplot(babies_data,aes(m_ht,baby_wt))+
+p_wt_mht <- ggplot(babies_data,aes(m_ht,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_m_ht <-cor.test(babies_data$m_ht,babies_data$baby_wt) 
 
 # relationship: baby weight and m_wt
-ggplot(babies_data,aes(m_wt,baby_wt))+
+p_wt_mwt <- ggplot(babies_data,aes(m_wt,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_m_wt <-cor.test(babies_data$m_wt,babies_data$baby_wt) 
 
 # relationship: baby weight and d_race
-ggplot(babies_data,aes(d_race,baby_wt))+
+p_wt_drace <- ggplot(babies_data,aes(d_race,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
-# Calculate the correlation coefficient and test whether the correlation is significant
-bbwt_d_race <-cor.test(babies_data$d_race,babies_data$baby_wt) 
 
 # relationship: baby weight and d_age
-ggplot(babies_data,aes(d_age,baby_wt))+
+p_wt_dage <- ggplot(babies_data,aes(d_age,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_d_age <-cor.test(babies_data$d_age,babies_data$baby_wt) 
 
 # relationship: baby weight and d_edu
-ggplot(babies_data,aes(d_edu,baby_wt))+
+p_wt_dedu <- ggplot(babies_data,aes(d_edu,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
-# Calculate the correlation coefficient and test whether the correlation is significant
-bbwt_d_edu <-cor.test(babies_data$d_edu,babies_data$baby_wt) 
 
 # relationship: baby weight and d_ht
-ggplot(babies_data,aes(d_ht,baby_wt))+
+p_wt_dht <- ggplot(babies_data,aes(d_ht,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_d_ht <-cor.test(babies_data$d_ht,babies_data$baby_wt) 
 
 # relationship: baby weight and d_wt
-ggplot(babies_data,aes(d_wt,baby_wt))+
+p_wt_dwt <- ggplot(babies_data,aes(d_wt,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_d_wt <-cor.test(babies_data$d_wt,babies_data$baby_wt) 
 
 # relationship: baby weight and income
-ggplot(babies_data,aes(income,baby_wt))+
+p_wt_inc <- ggplot(babies_data,aes(income,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_income <- cor.test(babies_data$income,babies_data$baby_wt) 
 
 # relationship: baby weight and smoke
-bbwt_smoke <- babies_data %>%
-  group_by(smoke) %>%
-  summarise(bbwt_mean = mean(baby_wt, na.rm = TRUE))
-bbwt_smoke <- kruskal.test(babies_data$baby_wt~babies_data$smoke,data=babies_data)
-
-# relationship: baby weight and smoke_time
-bbwt_smoke_time <- babies_data %>%
-  group_by(smoke_time) %>%
-  summarise(bbwt_mean = mean(baby_wt, na.rm = TRUE))
-bbwt_smoke_time <- kruskal.test(babies_data$baby_wt~babies_data$smoke_time,data=babies_data)
-
-# relationship: baby weight and cig_number
-ggplot(babies_data,aes(cig_number,baby_wt))+
+p_wt_smo <- ggplot(babies_data,aes(smoke,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
-# Calculate the correlation coefficient and test whether the correlation is significant
-bbwt_cig_number <-cor.test(babies_data$cig_number,babies_data$baby_wt) 
+
+# relationship: baby weight and smoke_time
+p_wt_smot <- ggplot(babies_data,aes(smoke_time,baby_wt))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+# relationship: baby weight and cig_number
+p_wt_cig <- ggplot(babies_data,aes(cig_number,baby_wt))+
+  geom_point()+
+  geom_smooth(method = "lm")
 
 # relationship: baby weight and previous_preg
-ggplot(babies_data,aes(previous_preg,baby_wt))+
+p_wt_preg <- ggplot(babies_data,aes(previous_preg,baby_wt))+
   geom_point()+
   geom_smooth(method = "lm")
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_previous_preg <-cor.test(babies_data$previous_preg,babies_data$baby_wt) 
 
 # relationship: baby weight and marital
-bbwt_marital <- babies_data %>%
-  group_by(marital) %>%
-  summarise(bbwt_mean = mean(baby_wt, na.rm = TRUE))
-bbwt_marital <- kruskal.test(babies_data$baby_wt~babies_data$marital,data=babies_data)
+p_wt_marital <- ggplot(babies_data,aes(marital,baby_wt))+
+  geom_point()+
+  geom_smooth(method = "lm")
 
-
+# Put the plots together
+multiplot(p_wt_ges,p_wt_mrace,p_wt_mage,p_wt_medu,p_wt_mht,p_wt_mwt,
+          p_wt_drace,p_wt_dage,p_wt_dedu,p_wt_dht,p_wt_dwt,p_wt_inc,
+          p_wt_smo,p_wt_smot,p_wt_cig,p_wt_preg,p_wt_marital,cols = 4)
