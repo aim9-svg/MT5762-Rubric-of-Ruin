7c6546f7a3d56ea61e7decc49181b1a04bde18a4
JingyiLi318 <56074065+JingyiLi318@users.noreply.github.com>
2019-11-03 08:39:41 +0000

    change the plots

--- MT5762 - Assignment 2.R
+++ MT5762 - Assignment 2.R
@@ -113,6 +113,18 @@ babies_data$d_edu[babies_data$d_edu == 3] = "HS+trade"
 babies_data$d_edu[babies_data$d_edu == 4] = "HS+college"
 babies_data$d_edu[babies_data$d_edu == 5] = "College graduate"
 
+# income 
+babies_data$income[babies_data$income == 0] = "$0-$2499"
+babies_data$income[babies_data$income == 1] = "$2500-$4999"
+babies_data$income[babies_data$income == 2] = "$4999-$7499"
+babies_data$income[babies_data$income == 3] = "$7500-$9999"
+babies_data$income[babies_data$income == 4] = "$10000-$12499"
+babies_data$income[babies_data$income == 5] = "$12500-$14999"
+babies_data$income[babies_data$income == 6] = "$15000-$17499"
+babies_data$income[babies_data$income == 7] = "$17500-$19999"
+babies_data$income[babies_data$income == 8] = "$20000-$22499"
+babies_data$income[babies_data$income == 9] = "$22500+"
+
 # marital
 babies_data$marital[babies_data$marital == 1] = "married"
 babies_data$marital[babies_data$marital == 2] = "legally separated"
@@ -129,13 +141,13 @@ babies_data$smoke[babies_data$smoke == 3] = "once did not now"
 # smoke_time (If mother quit, how long ago?)
 babies_data$smoke_time[babies_data$smoke_time == 0] = "never smoked"
 babies_data$smoke_time[babies_data$smoke_time == 1] = "still smokes"
-babies_data$smoke_time[babies_data$smoke_time == 2] = "during current preg"
+babies_data$smoke_time[babies_data$smoke_time == 2] = "during preg"
 babies_data$smoke_time[babies_data$smoke_time == 3] = "within 1 yr"
-babies_data$smoke_time[babies_data$smoke_time == 4] = "1 to 2 years ago"
-babies_data$smoke_time[babies_data$smoke_time == 5] = "2 to 3 yr ago"
-babies_data$smoke_time[babies_data$smoke_time == 6] = "3 to 4 yrs ago"
-babies_data$smoke_time[babies_data$smoke_time == 7] = "5 to 9yrs ago"
-babies_data$smoke_time[babies_data$smoke_time == 8] = "10+yrs ago"
+babies_data$smoke_time[babies_data$smoke_time == 4] = "1 to 2 years"
+babies_data$smoke_time[babies_data$smoke_time == 5] = "2 to 3 yr"
+babies_data$smoke_time[babies_data$smoke_time == 6] = "3 to 4 yrs"
+babies_data$smoke_time[babies_data$smoke_time == 7] = "5 to 9yrs"
+babies_data$smoke_time[babies_data$smoke_time == 8] = "10+yrs"
 
 # cig_number
 babies_data$cig_number[babies_data$cig_number == 0] = "0"
@@ -153,107 +165,227 @@ babies_data$cig_number[babies_data$cig_number == 8] = "60+"
 # relationship: baby weight and gestation
 p_wt_ges <- ggplot(babies_data,aes(gestation,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Gestation")+ylab("Baby Weight")+ggtitle("Relationship between gestation and baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_gestation <-cor.test(babies_data$gestation,babies_data$baby_wt)
 
 # relationship: baby weight and m_race
-p_wt_mrace <- ggplot(babies_data,aes(m_race,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+#p_wt_mrace <- ggplot(babies_data,aes(m_race,baby_wt))+
+  #geom_point()+
+  #geom_smooth(method = "lm")
+p_wt_mrace <- ggplot(babies_data,aes(m_race,baby_wt, fill = m_race))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Mother's race")+ylab("Baby weight")+ggtitle("Relationship between mother's race and Baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))+
+  scale_fill_discrete(name="Mother's race")
 
 # relationship: baby weight and m_age
 p_wt_mage <- ggplot(babies_data,aes(m_age,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Mother's age")+ylab("Baby Weight")+ggtitle("Relationship between mother's age and baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_m_age <-cor.test(babies_data$m_age,babies_data$baby_wt) 
 
 # relationship: baby weight and m_edu
-p_wt_medu <- ggplot(babies_data,aes(m_edu,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_medu <- ggplot(babies_data,aes(m_edu,baby_wt, fill = m_edu))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Mother's education")+ylab("Baby weight")+ggtitle("Relationship between mother's education and Baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 7))+
+  scale_fill_discrete(name="Mother's education")
 
 # relationship: baby weight and m_ht
 p_wt_mht <- ggplot(babies_data,aes(m_ht,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Mother's height")+ylab("Baby weight")+ggtitle("Relationship between mother's height and Baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_m_ht <-cor.test(babies_data$m_ht,babies_data$baby_wt) 
 
 # relationship: baby weight and m_wt
 p_wt_mwt <- ggplot(babies_data,aes(m_wt,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Mother's weight")+ylab("Baby weight")+ggtitle("Relationship between mother's weight and baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_m_wt <-cor.test(babies_data$m_wt,babies_data$baby_wt) 
 
 # relationship: baby weight and d_race
-p_wt_drace <- ggplot(babies_data,aes(d_race,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_drace <- ggplot(babies_data,aes(d_race,baby_wt, fill = d_race))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Father's race")+ylab("Baby weight")+ggtitle("Relationship between father's race and baby weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))+
+  scale_fill_discrete(name="Father's race")
 
 # relationship: baby weight and d_age
 p_wt_dage <- ggplot(babies_data,aes(d_age,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Father's Age")+ylab("Baby Weight")+ggtitle("Relationship between Father's Age and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_d_age <-cor.test(babies_data$d_age,babies_data$baby_wt) 
 
 # relationship: baby weight and d_edu
-p_wt_dedu <- ggplot(babies_data,aes(d_edu,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_dedu <- ggplot(babies_data,aes(d_edu,baby_wt, fill = d_edu))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Father's Education")+ylab("Baby Weight")+ggtitle("Relationship between Father's Education and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 7))+
+  scale_fill_discrete(name="Father's education")
 
 # relationship: baby weight and d_ht
 p_wt_dht <- ggplot(babies_data,aes(d_ht,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Father's Height")+ylab("Baby Weight")+ggtitle("Relationship between Father's Height and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_d_ht <-cor.test(babies_data$d_ht,babies_data$baby_wt) 
 
 # relationship: baby weight and d_wt
 p_wt_dwt <- ggplot(babies_data,aes(d_wt,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Father's Weight")+ylab("Baby Weight")+ggtitle("Relationship between Father's Weight and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_d_wt <-cor.test(babies_data$d_wt,babies_data$baby_wt) 
 
 # relationship: baby weight and income
-p_wt_inc <- ggplot(babies_data,aes(income,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
-# Calculate the correlation coefficient and test whether the correlation is significant
-bbwt_income <- cor.test(babies_data$income,babies_data$baby_wt) 
+p_wt_inc <- ggplot(babies_data,aes(income,baby_wt, fill = income))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Income")+ylab("Baby Weight")+ggtitle("Relationship between Income and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 5))+
+  scale_fill_discrete(name="Income")
 
 # relationship: baby weight and smoke
-p_wt_smo <- ggplot(babies_data,aes(smoke,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_smo <- ggplot(babies_data,aes(smoke,baby_wt, fill = smoke))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Smoking Status")+ylab("Baby Weight")+ggtitle("Relationship between Smoking status and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 7))+
+  scale_fill_discrete(name="Smoking Status")
 
 # relationship: baby weight and smoke_time
-p_wt_smot <- ggplot(babies_data,aes(smoke_time,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_smot <- ggplot(babies_data,aes(smoke_time,baby_wt, fill = smoke_time))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Quitting Time")+ylab("Baby Weight")+ggtitle("Relationship between Quitting Time and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 6))+
+  scale_fill_discrete(name="Quitting Time")
 
 # relationship: baby weight and cig_number
-p_wt_cig <- ggplot(babies_data,aes(cig_number,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_cig <- ggplot(babies_data,aes(cig_number, baby_wt, fill = cig_number))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Number of cigarettes smoked per day")+ylab("Baby Weight")+ggtitle("Relationship between Number of cigarettes smoked per day and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))+
+  scale_fill_discrete(name="Number")
 
 # relationship: baby weight and previous_preg
 p_wt_preg <- ggplot(babies_data,aes(previous_preg,baby_wt))+
   geom_point()+
-  geom_smooth(method = "lm")
+  geom_smooth(method = "lm")+
+  xlab("Previous pregnancies")+ylab("Baby Weight")+ggtitle("Relationship between Previous pregnancies and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 11))
 # Calculate the correlation coefficient and test whether the correlation is significant
 bbwt_previous_preg <-cor.test(babies_data$previous_preg,babies_data$baby_wt) 
 
 # relationship: baby weight and marital
-p_wt_marital <- ggplot(babies_data,aes(marital,baby_wt))+
-  geom_point()+
-  geom_smooth(method = "lm")
+p_wt_marital <- ggplot(babies_data,aes(marital,baby_wt, fill = marital))+
+  geom_boxplot(alpha = 0.7)+
+  xlab("Marital")+ylab("Baby Weight")+ggtitle("Relationship between Marital and Baby Weight")+
+  theme_bw()+
+  theme(plot.title = element_text(size = 13, face = "bold"),
+        text = element_text(size = 12),
+        axis.title = element_text(face = "bold"),
+        axis.text.x = element_text(size = 9))+
+  scale_fill_discrete(name="Marital")
 
-# Put the plots together
-multiplot(p_wt_ges,p_wt_mrace,p_wt_mage,p_wt_medu,p_wt_mht,p_wt_mwt,
-          p_wt_drace,p_wt_dage,p_wt_dedu,p_wt_dht,p_wt_dwt,p_wt_inc,
-          p_wt_smo,p_wt_smot,p_wt_cig,p_wt_preg,p_wt_marital,cols = 4)
+
+# Put the plots together(in report)
+p_wt_ges
+p_wt_inc
+p_wt_preg
+p_wt_marital
+multiplot(p_wt_mrace,p_wt_medu,cols = 2)
+multiplot(p_wt_mage,p_wt_mht,p_wt_mwt,cols = 3)
+multiplot(p_wt_drace,p_wt_dedu,cols = 2)
+multiplot(p_wt_dage,p_wt_dht,p_wt_dwt,cols = 3)
+multiplot(p_wt_smo,p_wt_smot,p_wt_cig,cols = 3)
+
+
+
+
+
+
+
+
+
+
